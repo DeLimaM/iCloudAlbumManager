5b7a93565d361a9a5e5e07832f92a6e0056a05e5
DeLimaM <martin.delimaa@gmail.com>
2023-10-22 21:45:10 +0200

    implemented login

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pyicloud
 
 app = Flask(__name__)
+api = None
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def root():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    try:
+        global api # This is a bad idea, but I'm lazy and this is a small project
+        api = pyicloud.PyiCloudService(username, password, cookie_directory='cookies')
+        if api.requires_2fa:
+            print("Two-factor authentication required.")
+            return render_template('2fa.html')
+        else:
+            if not api.is_trusted_session:
+                print("Trusting session...")
+                api.trust_session()
+            print("Logged in successfully as %s" % username)
+            return render_template('index.html')
+    except Exception as e:
+        print("Failed to log in as %s" % username)
+        return render_template('login.html')
+
+
+@app.route('/2fa', methods=['POST'])
+def two_factor():
+    code = request.form['code']
+    try:
+        result = api.validate_2fa_code(code)
+        if result:
+            if not api.is_trusted_session:
+                print("Trusting session...")
+                api.trust_session()
+            print("Two-factor authentication succeeded!")
+            return render_template('index.html')
+        else:
+            print("Two-factor authentication failed!")
+            return render_template('login.html')
+    except Exception as e:
+        print("Two-factor authentication failed!")
+        return render_template('login.html')
 
 
 if __name__ == '__main__':
